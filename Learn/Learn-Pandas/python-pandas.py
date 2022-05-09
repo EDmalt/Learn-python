908b35931ebb155108615e743a532ff35980ee13
EDmalt <2283157463@qq.com>
2022-05-09 20:24:20 +0800

    Update python-pandas.py

--- Learn/Learn-Pandas/python-pandas.py
+++ Learn/Learn-Pandas/python-pandas.py
@@ -1,7 +1,14 @@
 # By hefei
 # @File : python-pandas.py
-import pandas
+import pandas as pd
 
 a=[1,2,3]
 
-print(pandas.Series(a))
+print(pd.Series(a))
+
+#pd.to_csv('E:test.csv')
+print(pd.date_range("20200102",periods=10,freq="M"))
+
+data = [1,2,3,4,5]
+df = pd.DataFrame(data)
+print(df)
